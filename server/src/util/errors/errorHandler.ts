368b4f966dd61c819aa49f62f4b97afac3d73bb8
David Ethan Hwang <hdavidethan@gmail.com>
2021-08-13 17:21:15 -0400

    Fixed error handler

--- server/src/util/errors/errorHandler.ts
+++ server/src/util/errors/errorHandler.ts
@@ -4,7 +4,7 @@ import { SiteError } from './error';
 export const errorHandler = (err: SiteError, req: Request, res: Response, next: NextFunction): void => {
   console.log(err);
   res.render('error', {
-    root: process.env.ROOT_URL,
+    root: process.env.ROOT_URL || 'http://localhost:4000',
     errstatus: err.status,
     errdetail: err.detail,
     errmsg: err.msg,
