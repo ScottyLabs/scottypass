4a61cf2c9221e206252b265d613463e826a21cef
David Ethan Hwang <hdavidethan@gmail.com>
2022-02-03 23:08:43 -0500

    Removed options from mongoose connect

--- server/src/index.ts
+++ server/src/index.ts
@@ -19,7 +19,7 @@ import verifyCallback from './controller/login/verify';
 
 (async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI || '', { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect(process.env.MONGODB_URI || '');
   } catch (err) {
     console.error(err);
     process.exit(-1);
